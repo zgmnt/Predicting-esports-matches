77fd9d8140cd5ddbbe4427126d407a14b320476c
zgmnt <zygmunt.latyszewicz@outlook.com>
2022-07-03 13:07:22 +0200

    TBD fixed

--- scripts/UpcomingMatches.py
+++ scripts/UpcomingMatches.py
@@ -10,14 +10,19 @@ matchesWrapper = soup.find_all(class_="upcomingMatchesWrapper")
 matchesDaysUpcoming = soup.find_all(class_="upcomingMatchesSection")
 matchDayHeadline = soup.find_all(class_="matchDayHeadline")
 matchFrame = soup.find_all(class_="match a-reset")
+matchTeam = soup.find_all(class_="matchTeam")
 
-upcomingMatchesDays = []
+for elem in matchTeam:
+    indexBegin = str(elem).find("text-ellipsis")
+    indexEnd = str(elem).find("</div>")
+    print(str(elem)[indexBegin+len('text-ellipsis">'):].replace("</div>", "").replace("\n", ""))
+
 
 for i in range(len(matchDayHeadline)):
     stringLine = str(matchDayHeadline[i])
     indexDateBegin = stringLine.find(">")
     indexDateEnd = stringLine.find("</")
-    upcomingMatchesDays.append(stringLine[indexDateBegin + 1: indexDateEnd])
+    #upcomingMatchesDays.append(stringLine[indexDateBegin + 1: indexDateEnd])
 
 matchedDetailsURLs = []
 for i in range(len(matchFrame)):
@@ -29,36 +34,23 @@ matchedDetailsURLs.pop(0)
 pageMatch = requests.get(matchedDetailsURLs[0])
 soupMatch = BeautifulSoup(page.content, 'html.parser')
 
-# CHANGE FUNCTION NAME
-def find_all_in_soup(ListOfValues, soup):
-    matches = []
-    teamsRaw = []
-    teams = []
-    matches = soup.find_all(class_="upcomingMatch")
-    for match in matches:
-        teamsRaw.append(match.find(class_="matchTeam team1"))
-        teamsRaw.append(match.find(class_="matchTeam team2"))
 
-    for teamRaw in teamsRaw:
-        teamRaw.find(class_="")
-
-
-
-teamNames = soupMatch.find_all(class_="matchTeamName")
+teamNames = soupMatch.find_all(class_="text-ellipsis")
 teamsInMatchTable = []
 teamsInMatchTuples = []
-
-find_all_in_soup(["matchTeam team1"], soup)
 
 for i in range(len(teamNames)):
     indexBegin = str(teamNames[i]).find('">')
     indexEnd = str(teamNames[i]).find("</div>")
     teamsInMatchTable.append(str(teamNames[i])[indexBegin + 2: indexEnd])
 
+
+
+"""
 for i in range(0, len(teamNames), 2):
     teamsInMatchTuples.append(tuple((teamsInMatchTable[i], teamsInMatchTable[i+1])))
 
-"""
+
 with open("..//data//UpcomingMatches.csv", "w") as file:
     file.write("Team A,Team B\n")
     for tuple_ in teamsInMatchTuples:
