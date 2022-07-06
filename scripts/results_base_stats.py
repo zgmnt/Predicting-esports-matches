da417dfe701fdc5072de9fa56e357d689c9bdc70
zgmnt <zygmunt.latyszewicz@outlook.com>
2022-07-06 14:22:05 +0200

    fixed shift with big results inside link generator function

--- scripts/results_base_stats.py
+++ scripts/results_base_stats.py
@@ -14,7 +14,8 @@ def get_links_match_pages(url_, pages_amount):
     results_links = []
     offset_link = "?offset="
     for i in range(int(pages_amount)):
-        results_raw = url_to_soup(url_).find_all(class_="result-con")
+        soup = url_to_soup(url_)
+        results_raw = soup.find(class_="results-holder allres").find_all(class_="result-con")
         for elem in results_raw:
             index_begin = str(elem).find('href="/matches')
             index_end = str(elem).find('<div class="result">')
@@ -35,7 +36,7 @@ def get_teams(matches_links):
         teams_name_raw = soup.find_all(class_="teamName")
         print(i, "teams")
         if i % 2:  # error 1015 handle
-            time.sleep(0.5)
+            time.sleep(0.3)
         for j in range(2):
             if len(list(teams_name_raw)) != 0:
                 team_name = list(teams_name_raw)[j]
@@ -83,7 +84,7 @@ def get_maps(matches_links):
         temp.clear()
 
         if i % 2:
-            time.sleep(0.5)
+            time.sleep(0.3)
 
     return maps
 
@@ -106,45 +107,37 @@ def get_scores(matches_links):
                     score_a.append(score_fixed)
 
         if i % 2:
-            time.sleep(0.5)
+            time.sleep(0.3)
 
     return score_a, score_b
 
 
-def get_big_results_length(url):
-    soup = url_to_soup(url)
-    big_results = soup.find(class_="big-results").find_all(class_="result-con")
-    return len(big_results)
-
-
-def to_csv_teams_maps(skip_index,path_, filename_, team_a, team_b, maps):
+def to_csv_teams_maps(path_, filename_, team_a, team_b, maps):
     with open(("%s%s%s" % (path_, filename_, ".csv")), "w", newline="") as file:
         file.write("%s,%s,%s%s" % ("teamA", "teamB", "map", "\n"))
-        for i in range(skip_index,len(team_a)):
+        for i in range(len(team_a)):
             for j in range(len(list(maps.values())[i])):
                 file.write("%s,%s,%s\n" % (team_a[i], team_b[i], list(maps.values())[i][j]))
 
 
-def to_csv_scores(skip_index, path_, filename_, score_a, score_b):
+def to_csv_scores(path_, filename_, score_a, score_b):
     with open(("%s%s%s" % (path_, filename_, ".csv")), "w", newline="") as file:
         file.write("%s,%s%s" % ("scoreTeamA", "scoreTeamB", "\n"))
-        for i in range(skip_index, len(score_a)):
+        for i in range(len(score_a)):
             file.write("%s,%s\n" % (score_a[i], score_b[i]))
 
 
 URL = "https://www.hltv.org/results"
-PAGES_AMOUNT = 1
+PAGES_AMOUNT = 3
 MATCHES_LINKS = get_links_match_pages(URL, PAGES_AMOUNT)
 TEAM_A, TEAM_B = get_teams(MATCHES_LINKS)
 MAPS = get_maps(MATCHES_LINKS)
 SCORE_A, SCORE_B = get_scores(MATCHES_LINKS)
-SKIP_INDEX = get_big_results_length(URL)
-print(SKIP_INDEX)
 # generate csv
 
 RESULTS_PATH = "..//data//results//"
-to_csv_teams_maps(SKIP_INDEX, RESULTS_PATH, "results_teams_maps", TEAM_A, TEAM_B, MAPS)
-to_csv_scores(SKIP_INDEX, RESULTS_PATH, "results_scores", SCORE_A,SCORE_B)
+to_csv_teams_maps(RESULTS_PATH, "results_teams_maps", TEAM_A, TEAM_B, MAPS)
+to_csv_scores(RESULTS_PATH, "results_scores", SCORE_A,SCORE_B)
 
 
 tab1 = pd.read_csv("..//data//results//results_teams_maps.csv")
